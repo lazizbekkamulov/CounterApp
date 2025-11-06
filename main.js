1af09a50a3be11a90e951d977503849e4e95c858
oktamovshokhjahon <oktamovshokhjahon@example.com>
2025-11-06 16:27:42 +0500

    finish

---
+++ main.js
@@ -0,0 +1,18 @@
+const countSpan = document.getElementById("count");
+const plusBtn = document.getElementById("plusBtn");
+const minusBtn = document.querySelector("#minusBtn");
+let count = 0;
+
+const sdd = () => {};
+
+plusBtn.addEventListener("click", () => {
+  count += 1;
+  countSpan.textContent = count;
+});
+
+minusBtn.addEventListener("click", () => {
+  if (count > 0) {
+    count -= 1;
+    countSpan.textContent = count;
+  }
+});
